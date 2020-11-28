6438805012d6ada107166f336c6464f6ad70bc7b
Victor <victor-2122@hotmail.com>
2020-11-28 17:26:50 -0300

    Terminado a inserção de endereço no CardView

--- app/src/main/java/com/example/trips/LugarViewHolder.java
+++ app/src/main/java/com/example/trips/LugarViewHolder.java
@@ -9,9 +9,11 @@ public class LugarViewHolder extends RecyclerView.ViewHolder{
 
     TextView nomeTextView;
     TextView dataTextview;
+    TextView enderecoTextview;
     LugarViewHolder(View v){
         super (v);
         this.nomeTextView = v.findViewById(R.id.nomeTextView);
         this.dataTextview = v.findViewById(R.id.dataTextView);
+        this.enderecoTextview = v.findViewById(R.id.enderecoTextView);
     }
 }
