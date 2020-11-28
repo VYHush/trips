b9c5754ebbb363784418a343f710dc41746c82ee
Victor <victor-2122@hotmail.com>
2020-11-28 15:49:09 -0300

    Iniciado a inclusão do endereço na visualizaçao do lugar

--- app/src/main/java/com/example/trips/LugarAdapter.java
+++ app/src/main/java/com/example/trips/LugarAdapter.java
@@ -1,6 +1,8 @@
 package com.example.trips;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +13,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class LugarAdapter extends RecyclerView.Adapter <LugarViewHolder>{ private
     List<Lugar> lugares;
     private Context context;
     private CollectionReference lugarReference = null;
+    Geocoder geocoder;
+    List<Address> addresses;
     LugarAdapter (List<Lugar> lugares, Context context){
         this.lugares = lugares;
         this.context = context;
@@ -35,6 +41,14 @@ public class LugarAdapter extends RecyclerView.Adapter <LugarViewHolder>{ privat
         Lugar l = lugares.get(position);
         holder.dataTextview.setText(l.getDataCadastro());
         holder.nomeTextView.setText(l.getNome());
+
+        geocoder = new Geocoder(this.context, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(Double.parseDouble(l.getLatitude()), Double.parseDouble(l.getLongitude()), 1);
+            System.out.println(addresses.get(0).getThoroughfare() + addresses.get(0).getSubThoroughfare());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         holder.itemView.setOnLongClickListener(new View.OnLongClickListener(){
             @Override
             public boolean onLongClick(View v) {
